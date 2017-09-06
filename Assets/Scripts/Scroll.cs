7d0ed9dc5aa24990220f110cdc7a4389a58db1b6
dariofg93 <dariofg93@gmail.com>
2017-09-06 12:27:40 -0300

    scroll de background

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+	public float velocidad = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (0, Time.time * velocidad);
+	}
+}
