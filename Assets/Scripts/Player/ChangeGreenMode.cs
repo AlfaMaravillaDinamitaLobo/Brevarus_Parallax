94cfd9fbcb4bb3d9b8e1c25a1941a60f37a07d6d
dariofg93 <dariofg93@gmail.com>
2017-09-23 21:41:48 -0300

    reorganizacion del proyecto y cambio de modos de disparo

---
+++ Assets/Scripts/Player/ChangeGreenMode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGreenMode : MonoBehaviour {
+
+	private Animator animacion;
+
+	// Use this for initialization
+	void Start () {
+		animacion = GetComponent<Animator> ();
+		animacion.SetBool ("redMode", true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Alpha2) && animacion.GetBool ("redMode")) {
+			animacion.SetBool ("greenMode", true);
+			animacion.SetBool ("redMode", false);
+		}
+
+		if (Input.GetKey (KeyCode.Alpha1) && animacion.GetBool ("greenMode")) {
+			animacion.SetBool ("redMode", true);
+			animacion.SetBool ("greenMode", false);
+		}
+	}
+}
