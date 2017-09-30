6e667aa52c84c5521ab1125a9b8c606bd9748797
dariofg93 <dariofg93@gmail.com>
2017-09-30 17:39:13 -0300

    miniboss animation

---
+++ Assets/Scripts/Enemy/Enemy2Mov.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy2Mov : MonoBehaviour {
+
+	float velocidadMovimiento = 8f;
+	bool llegoAlFinal;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (llegoAlFinal) {
+			transform.position += Vector3.right * velocidadMovimiento * Time.deltaTime;
+		} 
+		else {
+			transform.position -= Vector3.right * velocidadMovimiento * Time.deltaTime;
+		}
+
+		if (transform.position.x >= 13f) {
+			llegoAlFinal = false;
+		}
+
+		if (transform.position.x <= -13f) {
+			llegoAlFinal = true;
+		}
+	}
+}
