0aed260fd777e3182ead4e92296e281385b6e567
BrianLeoGoldman <leonel89011@gmail.com>
2017-12-02 22:31:00 -0300

    Creado disparo especial de la cabeza del boss

---
+++ Assets/Scripts/Final Boss/HeadShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadShooting : MonoBehaviour {
+
+	public ParticleSystem particles;
+	public int specialInterval;
+	private int specialCounter;
+
+	void Start () {
+		specialCounter = specialInterval;
+	}
+
+	void Update () {
+		if (specialCounter < 0) {
+			FireBeam ();
+			specialCounter = specialInterval;
+		}
+		specialCounter--;
+		
+	}
+
+	public void FireBeam(){
+		Debug.Log ("Special Boss Attack");
+		Instantiate (particles, transform);
+	}
+}
